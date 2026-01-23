012c40112f2b700a9ed3e78c2739efc63dad063e
Thiago Pessoa <tpessoa96@gmail.com>
2026-01-22 21:12:11 -0300

    Alterando permissão de mudança de senha

--- src/main/java/com/mballen/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballen/demo_park_api/web/controller/UsuarioController.java
@@ -74,6 +74,7 @@ public class UsuarioController {
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
             })
     @PatchMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENTE') AND (#id == authentication.principal.id)")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody @Valid UsuarioSenhaDto dto){
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return  ResponseEntity.noContent().build();
