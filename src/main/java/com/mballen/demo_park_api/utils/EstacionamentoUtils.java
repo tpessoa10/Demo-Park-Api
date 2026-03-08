403d4ce683e5ea56d8778ae4b93fb1f37dd7a4c7
Thiago Pessoa <tpessoa96@gmail.com>
2026-03-08 09:00:52 -0300

    Estacionamento utils e método gerarRecibo

---
+++ src/main/java/com/mballen/demo_park_api/utils/EstacionamentoUtils.java
@@ -0,0 +1,18 @@
+package com.mballen.demo_park_api.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+    public static String gerarRecibo(){
+        LocalDateTime date = LocalDateTime.now();
+        String recibo = date.toString().substring(0, 19);
+        return recibo.replace("-","")
+                .replace(":","")
+                .replace(" T","-");
+    }
+}
