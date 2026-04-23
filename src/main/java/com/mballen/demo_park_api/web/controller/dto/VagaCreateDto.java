f883b4c3129c4cfc9414e9267ede2c662f048db0
Thiago Pessoa <tpessoa96@gmail.com>
2026-04-22 21:35:47 -0300

    Internacionalização de mensagens.

--- src/main/java/com/mballen/demo_park_api/web/controller/dto/VagaCreateDto.java
+++ src/main/java/com/mballen/demo_park_api/web/controller/dto/VagaCreateDto.java
@@ -14,10 +14,10 @@ import lombok.Setter;
 @AllArgsConstructor
 public class VagaCreateDto {
 
-    @NotBlank
-    @Size(min = 4, max = 4)
+    @NotBlank(message = "{NotBlank.vagaCreateDto.codigo}")
+    @Size(min = 4, max = 4, message = "{Size.vagaCreateDto.codigo}")
     private String codigo;
-    @NotBlank
-    @Pattern(regexp = "LIVRE|OCUPADA")
+    @NotBlank(message = "{NotBlank.vagaCreateDto.status}")
+    @Pattern(regexp = "LIVRE|OCUPADA", message = "{Pattern.vagaCreateDto.status}")
     private String status;
 }
--- src/main/java/com/mballen/demo_park_api/web/controller/exception/ApiExceptionHandler.java
+++ src/main/java/com/mballen/demo_park_api/web/controller/exception/ApiExceptionHandler.java
@@ -2,7 +2,9 @@ package com.mballen.demo_park_api.web.controller.exception;
 
 import com.mballen.demo_park_api.exception.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +17,19 @@ import java.nio.file.AccessDeniedException;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ApiExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessage> methodArgumentValidException(MethodArgumentNotValidException ex, HttpServletRequest request,
                                                                      BindingResult result) {
         log.error("API ERROR:", ex);
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campos inválidos!", result));
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY,
+                        messageSource.getMessage("message.invalid.field", null, request.getLocale()),
+                        result, messageSource));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
